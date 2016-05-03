816741e9a213e4fbf9577e82eeeb3defc2c3d601
Алексей <agragregra@ya.ru>
2016-05-03 20:53:33 +0700

    Commit

--- catalogFilter.php
+++ catalogFilter.php
@@ -1,10 +1,10 @@
 <?php
 //Filter Fields Settings
+$filter = array();
 
 //Radio, Select & Text Fields Type
-$filter = array();
 if($_GET['floor']) {
-	$filter[] = 'floor=='.$_GET['floor'];
+	$filter[] = 'floor='.$_GET['floor'];
 }
 
 //Two Text Fields From To
@@ -17,7 +17,7 @@ if($_GET['area_to']) {
 
 //Checkbox Type
 if($_GET['garage']) {
-	$filter[] = 'garage==1';
+	$filter[] = 'garage=1';
 }
 
 //End Settings
@@ -41,14 +41,21 @@ if($_GET['offset']){
 	$offset = $_GET['offset'];
 }
 
+if($filter) {
+	$where = $modx->toJSON(array($filter));
+} else {
+	$where = '';
+}
+
 $params_count = array(
 	'parents' => $parents,
 	'limit' => 0,
 	'tpl' => '@INLINE ,',
 	'select' => 'id',
 	'showHidden' => '1',
-	'tvFilters' => implode(',',$filter)
+	'where' => $where
 	);
+
 $count = $modx->runSnippet('pdoResources',$params_count);
 $count = count(explode(',',$count))-1;
 $modx->setPlaceholder('count',$count);
@@ -63,7 +70,7 @@ $params = array(
 	'showHidden' => '1',
 	'sortby' => $sortby,
 	'sortdir' => $sortdir,
-	'tvFilters' => implode(',',$filter)
+	'where' => $where
 	);
 
 $more = $count - $offset - $limit;
@@ -71,7 +78,7 @@ $lim = $more > $limit ? $limit : $more;
 
 $button = '';
 if($more > 0){
-	$button = '<div class="ajax-filter-count" data-count="'.$count.'"><a href="#" class="ajax-more">Загрузить еще '.$lim.' из '.$more.'</a></div>';
+	$button = '<div class="ajax-filter-count col-sm-12" data-count="'.$count.'"><a href="#" class="btn-lg btn-primary ajax-more">Загрузить еще '.$lim.' из '.$more.'</a></div>';
 }
 
 return $modx->runSnippet('pdoResources',$params).$button;
